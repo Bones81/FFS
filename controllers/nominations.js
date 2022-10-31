f04cb494daa80cc38c32e40349efa34e7c0c3703
Nathan Freeman <nathanlfreeman@gmail.com>
2022-10-31 04:01:20 -0400

    begin building nomination index and new routes

--- controllers/nominations.js
+++ controllers/nominations.js
@@ -3,7 +3,52 @@ const router = express.Router()
 
 const Nomination = require('../models/nominations')
 const nominationSeed = require('../models/seed_nominations')
+const Screening = require('../models/screening')
+const Movie = require('../models/movies')
 
+const genres = [
+  'Action',
+  'Adventure',
+  'Animated',
+  'Art Film',
+  'Comedy',
+  'Comic Book-Based',
+  'Crime',
+  'Drama',
+  'Experimental',
+  'Fantasy',
+  'Historical',
+  'Musical',
+  'Mystery',
+  'Horror',
+  'Romance',
+  'Satire',
+  'Science Fiction',
+  'Thriller',
+  'Video Game-Based',
+  'Western',
+  'Other'
+]
+
+const nominators = [
+  'Nathan Freeman',
+  'Sujan Trivedi',
+  'Stephanie Weitzner',
+  'Nigam Trivedi',
+  'Walter J. McMath',
+  'John Vennema',
+  'Katharine Croke',
+  'Paul Ziemba',
+  'Emily Ziemba',
+  'Carmen Roman',
+  'Ben Beckley-Chayes',
+  'Jeffrey Kitrosser',
+  'Salman Baig',
+  'Jeremy Weissmann',
+  'Micah Baskir',
+  'Brain Trust',
+  'Ziemba Household',
+]
 
 //JSON
 router.get('/json', (req, res) => {
@@ -15,19 +60,33 @@ router.get('/json', (req, res) => {
 
 //INDEX
 router.get('/', (req, res) => {
-    Nomination.find({}, (err, nominations) => {
-      res.render('nominations/index.ejs', {
-        tabTitle: 'FFS Nominations',
-        nominations: nominations
-      })
+  Screening.find({}, (err, screenings) => {
+    Movie.find({}, (err, movies) => {
+      Nomination.find({}, (err, nominations) => {
+        res.render('nominations/index.ejs', {
+          tabTitle: 'FFS Nominations',
+          nominations: nominations,
+          screenings: screenings,
+          movies: movies
+        })
+      }) 
     })
+  })
 })
 
 //NEW
 router.get('/new', (req, res) => {
-    res.render('nominations/new.ejs', {
-      tabTitle: 'Add Nomination'
+  Screening.find({}, (err, screenings) => {
+    Movie.find({}, (err, movies) => {
+      res.render('nominations/new.ejs', {
+        tabTitle: 'Add Nomination',
+        screenings: screenings,
+        movies: movies,
+        genres: genres,
+        nominators: nominators
+      })
     })
+  })
 })
 
 //CREATE
