ac6b3fcbb8a9153459901eb67c67dc9b23f745ee
Nathan Freeman <nathanlfreeman@gmail.com>
2023-03-10 14:53:53 -0500

    add script.js partial to all view pages (except maintenance.ejs); fine tune z-index and position settings to ensure desired behavior; fine tune table header sticky position values.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    alert('script tag works!') 
+    console.log('JS Script works.');
+    const mainNav = document.querySelector('.main-nav')
+    const mobileNav = document.querySelector('.mobile-nav')
+    const hamburger = document.querySelector('.hamburger')
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('open')
+        mobileNav.classList.toggle('active')
+
+        if (hamburger.classList.contains('open')) {
+            hamburger.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+        } else {
+            hamburger.innerHTML = '<i class="fa-sharp fa-solid fa-bars"></i>'
+        }
+    })
+
 })
